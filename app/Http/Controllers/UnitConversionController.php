4207436eba6a652c6c4c620a9e8ae7255a5f3b6f
bimanyunugroho <devabi661528@gmail.com>
2025-04-19 17:24:24 +0700

    feat(migrations): migrate of product, unit and unit conversion

---
+++ app/Http/Controllers/UnitConversionController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\UnitConversion;
+use App\Http\Requests\StoreUnitConversionRequest;
+use App\Http\Requests\UpdateUnitConversionRequest;
+
+class UnitConversionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreUnitConversionRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(UnitConversion $unitConversion)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(UnitConversion $unitConversion)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateUnitConversionRequest $request, UnitConversion $unitConversion)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(UnitConversion $unitConversion)
+    {
+        //
+    }
+}
