ea90bd4f5936eba2445e6c41734f6e55951c66d4
bimanyunugroho <devabi661528@gmail.com>
2025-04-20 15:26:38 +0700

    install: add dependency pinia, vue-toast, vue-select

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -6,21 +6,13 @@ import type { DefineComponent } from 'vue';
 import { createApp, h } from 'vue';
 import { ZiggyVue } from 'ziggy-js';
 import { initializeTheme } from './composables/useAppearance';
-
-// Extend ImportMeta interface for Vite...
-declare module 'vite/client' {
-    interface ImportMetaEnv {
-        readonly VITE_APP_NAME: string;
-        [key: string]: string | boolean | undefined;
-    }
-
-    interface ImportMeta {
-        readonly env: ImportMetaEnv;
-        readonly glob: <T>(pattern: string) => Record<string, () => Promise<T>>;
-    }
-}
+import { createPinia } from 'pinia';
+import Toast from "vue-toastification";
+import "vue-toastification/dist/index.css";
+import { VueSelect } from "vue-select";
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+const pinia = createPinia();
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
@@ -29,6 +21,9 @@ createInertiaApp({
         createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue)
+            .use(pinia)
+            .use(Toast, {})
+            .component("v-select", VueSelect)
             .mount(el);
     },
     progress: {
